54e1c3389b7df3c8e98f1077c36c24f337f3d64b
JesusD2405 <jesusdavid2405@gmail.com>
2025-02-09 20:25:23 -0400

    feature: adding call to order from preview order page with refactor in data typing

--- src/app/order-preview/page.tsx
+++ src/app/order-preview/page.tsx
@@ -1,13 +1,43 @@
+"use client";
+import { NextPage } from "next";
+import { Suspense, useEffect, useState } from "react";
+
+// Core
+import { OrderRepository } from "@/core/order/order.repository";
+import { OrderModel } from "@/core/order/order.model";
+
+// Components
 import { HeaderOrder } from "./components/HeaderOrder";
 import { ContentOrder } from "./components/ContentOrder";
 import { FooterOrder } from "./components/FooterOrder";
 
-export default function OrderPreview() {
+const OrderPreview: NextPage = () => {
+  const [order, setOrder] = useState<OrderModel | null>(null);
+
+  const getOrder = async () => {
+    try {
+      const orderRepository = new OrderRepository();
+      const orderData = await orderRepository.get();
+      console.log("-- orderData ", orderData);
+      setOrder(orderData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getOrder();
+  }, []);
+
   return (
     <div className="m-5 p-5 rounded overflow-hidden shadow-lg bg-white">
-      <HeaderOrder />
-      <ContentOrder />
-      <FooterOrder />
+      <Suspense fallback={<p>Loading Orden...</p>}>
+        <HeaderOrder />
+        <ContentOrder />
+        <FooterOrder />
+      </Suspense>
     </div>
   );
-}
+};
+
+export default OrderPreview;
